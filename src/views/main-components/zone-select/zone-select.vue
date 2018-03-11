ba89d9d80e1482d65bfc7f7998d609d599fd928a
wanghuan <1360527082@qq.com>
2018-03-11 19:48:04 +0800

    王焕

---
+++ src/views/main-components/zone-select/zone-select.vue
@@ -0,0 +1,55 @@
+<template>
+    <div><Cascader :data="data" :load-data="loadData" :render-format="format" ></Cascader>
+    <div>{{cunrrentId}}</div>
+    </div>
+    
+</template>
+<script>
+export default {
+    data () {
+        return {
+            cunrrentId: 1,
+            getUrl: 'http://localhost:8081/zone/getZoneByParentId/',
+            data: []
+        }
+    },
+    methods: {
+        init () {
+            this.$axios.get(this.getUrl + 1)
+                .then(response => {
+                    this.data=response.data;
+                    this.data.forEach(element=>{
+                        element.label = element.name
+                        if(element.level<4){element.children=[]}
+                        element.loading=false
+                    })
+                })
+        },
+        format (labels, selectedData) {
+                const index = labels.length - 1
+                const data = selectedData[index] || false
+                this.cunrrentId = data.id
+            },
+        loadData (item, callback) {
+            item.loading = true;
+            this.$axios.get(this.getUrl + item.id)
+                .then(response => {
+                    item.children=response.data;
+                    item.children.forEach(element=>{
+                        element.label = element.name
+                        if(element.level<3){
+                            element.children=[]
+                            element.loading=false
+                        }
+                    })
+                })
+            item.loading = false
+            callback()
+        }
+
+    },
+    mounted () {
+        this.init();
+    }
+}
+</script>
--- src/views/news/news.vue
+++ src/views/news/news.vue
@@ -4,25 +4,26 @@
 
 <template>
   <div class="inner">
+
     <div class="list-news">
       <div v-for="obj in news">
         <news-card :message="obj"></news-card>
-
       </div>
       <div class="page">
         <a @click="getNews" class="load-more">浏览更多</a>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
+import zoneCard from '@/views/main-components/zone-select/zone-select.vue'
 import newsCard from './components/news-card.vue'
 export default {
   name: 'information_index',
   components: {
-    newsCard
+    newsCard,
+    zoneCard
   },
   data () {
     return {
@@ -36,7 +37,7 @@ export default {
       this.getNews();
     },
     getNews () {
-      this.$axios.get(this.getUrl + '?pageNum=' + (parseInt(this.news.length / 5) + 1) + '&next=5')
+      this.$axios.get(this.getUrl + '?has=' + this.news.length + '&next=5')
         .then(response => {
             console.log(this.news)
             response.data.forEach(element => {
